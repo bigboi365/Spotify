d5fd51170489171bef56fdb0d74c759d3d50f688
bigboi365 <bigboi365@users.noreply.github.com>
2020-05-29 09:35:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 
-import spotipy
-import sys
-import pprint
 from spotipy.oauth2 import SpotifyClientCredentials
-
-if len(sys.argv) > 1:
-    search_str = sys.argv[1]
-else:
-    search_str = 'Radiohead'
+import spotipy
+from pprint import pprint
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-result = sp.search(search_str)
-pprint.pprint(result)
+
+pl_id = 'spotify:playlist:5RIbzhG2QqdkaP24iXLnZX'
+offset = 0
+
+while True:
+    response = sp.playlist_tracks(pl_id,
+                                  offset=offset,
+                                  fields='items.track.id,total')
+    pprint(response['items'])
+    offset = offset + len(response['items'])
+    print(offset, "/", response['total'])
+
+    if len(response['items']) == 0:
+        break
