e30f4521d6821d3e583733bfc4d1387561477ae3
Mikadocrea <michael.wauthelet@adimaker.yncrea.fr>
2020-05-29 09:13:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-set SPOTIPY_CLIENT_ID=fe89983598724afcbcd7d562104460e6
-set SPOTIPY_CLIENT_SECRET=6bf650c749c145a581b6d2b73fc5790c
 
 import spotipy
+import sys
+import pprint
 from spotipy.oauth2 import SpotifyClientCredentials
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+if len(sys.argv) > 1:
+    search_str = sys.argv[1]
+else:
+    search_str = 'Radiohead'
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
-
-for album in albums:
-    print(album['name'])
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+result = sp.search(search_str)
+pprint.pprint(result)
