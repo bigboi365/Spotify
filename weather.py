20bc0c4b941dfd2ecf6083ec6f9adaddc55cd072
Mikadocrea <michael.wauthelet@adimaker.yncrea.fr>
2020-06-11 16:39:02 +0200

    r

--- weather.py
+++ weather.py
@@ -54,23 +54,23 @@ def get_weather_data(url):
     parser = argparse.ArgumentParser(description="Quick Script for Extracting Weather data using Google Weather")
     parser.add_argument("region", nargs="?", help="""#Region to get weather for, must be available region.
                                         #Default is your current location determined by your IP Address""", default="")
-    """# parse arguments
-    args = parser.parse_args()
-    region = args.region
-    URL += region
+    #parse arguments
+    #args = parser.parse_args()
+    #region = args.region
+    #URL += region
     # get data
-    data = get_weather_data(URL)
+    #data = get_weather_data(URL)
     # print data
-    print("Météo pour:", data["region"])
-    print("Maintenant:", data["dayhour"])
-    print(f"Température maintenant: {data['temp_now']}°C")
-    print("Description:", data['weather_now'])
-    print("Précipitations:", data["precipitation"])
-    print("Humidité:", data["humidity"])
-    print("Vent:", data["wind"])
-    print("Prochains jours:")
-    for dayweather in data["next_days"]:
-        print("="*40, dayweather["name"], "="*40)
-        print("Description:", dayweather["weather"])
-        print(f"Température max: {dayweather['max_temp']}°C")
-        print(f"Température min: {dayweather['min_temp']}°C")"""
+    #print("Météo pour:", data["region"])
+    #print("Maintenant:", data["dayhour"])
+    #print(f"Température maintenant: {data['temp_now']}°C")
+    #print("Description:", data['weather_now'])
+    #print("Précipitations:", data["precipitation"])
+    #print("Humidité:", data["humidity"])
+    #print("Vent:", data["wind"])
+    #print("Prochains jours:")
+    #for dayweather in data["next_days"]:
+    #    print("="*40, dayweather["name"], "="*40)
+    #    print("Description:", dayweather["weather"])
+     #   print(f"Température max: {dayweather['max_temp']}°C")
+    #    print(f"Température min: {dayweather['min_temp']}°C")"""
